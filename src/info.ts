adabbc680c72ed87b97426fc2dda5e5bb8891050
Jonathan Cobb <jonathan@kyuss.org>
2023-08-26 13:13:51 -0400

    add initialize

--- src/info.ts
+++ src/info.ts
@@ -2,9 +2,8 @@ import { MediaPlugin } from "yuebing-media";
 import { OP_MAP, OPERATIONS, DEFAULT_PROFILES } from "./common.js";
 import { load } from "./op/mediainfo.js";
 
-load();
-
 export const mediaPlugin: MediaPlugin = {
+    initialize: load,
     operations: () => OPERATIONS,
     operationFunction: (op: string) => OP_MAP[op],
     operationConfigType: () => undefined,
